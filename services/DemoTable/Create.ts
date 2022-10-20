db2a4dda104d696fbc9dc91e37c5a61f87caa96b
Sourav Bhattacharjee <sourav.b@thoughtworks.com>
2022-10-21 00:57:37 +0530

    [Sourav] Add. Handle dynamo DB integration

---
+++ services/DemoTable/Create.ts
@@ -0,0 +1,33 @@
+import { DynamoDB } from 'aws-sdk';
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import { v4 } from 'uuid';
+
+const dbClient = new DynamoDB.DocumentClient();
+
+async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+    const result: APIGatewayProxyResult = {
+        statusCode: 200,
+        body: 'Hello from DynamoDB'
+    }
+
+    const item = typeof event.body === 'object'? event.body: JSON.parse(event.body);
+    item.id = v4();
+
+
+    try {
+        await dbClient.put({
+            TableName: 'DemoTable',
+            Item: item
+        }).promise()
+    }
+    catch (error: any) {
+        result.body = error.message;
+    }
+    result.body = JSON.stringify(`Created item with id ${item.id}`);
+
+    return result;
+}
+
+
+
+export { handler }
