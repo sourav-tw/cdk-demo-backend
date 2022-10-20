c544f647a7df5c66597a194212abcd35a39a62f2
Sourav Bhattacharjee <sourav.b@thoughtworks.com>
2022-10-20 22:09:34 +0530

    [Sourav] Add. Handle lambda with typescript

--- infrastructure/DemoStack.ts
+++ infrastructure/DemoStack.ts
@@ -4,27 +4,35 @@ import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambd
 import { join } from 'path';
 import {LambdaIntegration, RestApi} from 'aws-cdk-lib/aws-apigateway';
 import { GenericTable } from './GenericTable';
+import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
+import {PolicyStatement} from 'aws-cdk-lib/aws-iam';
 
 export class DemoStack extends Stack {
 
-    private api = new RestApi(this,'DemoAPI',{
-        restApiName: 'DemoAPI'
+    private lambdaApi = new RestApi(this,'DemoLambdaAPI',{
+        restApiName:'DemoLambdaAPI'
     });
+
     private demoTable = new GenericTable('DemoTable', 'id', this);
 
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props);
 
-        const helloLambda = new LambdaFunction(this, 'HelloLambda', {
-            runtime: Runtime.NODEJS_16_X,
-            code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
-            handler: 'hello.main',
-            functionName: 'HelloLamda'
+        const helloLambdaNodeJS = new NodejsFunction(this, 'helloLambdaNodeJS',{
+            entry: (join(__dirname,'..','services','node-lambda','hello.ts')),
+            handler: 'handler',
+            functionName: 'helloLambdaNodeJS'
         });
 
-        const helloLambdaIntegration = new LambdaIntegration(helloLambda);
-        const helloLambdaResource = this.api.root.addResource('hello');
-        helloLambdaResource.addMethod('GET', helloLambdaIntegration);
+
+        const s3ListPolicy = new PolicyStatement();
+        s3ListPolicy.addActions('s3:ListAllMyBuckets');
+        s3ListPolicy.addResources('*');
+        helloLambdaNodeJS.addToRolePolicy(s3ListPolicy);
+
+        const helloLambdaNodeJSIntegration = new LambdaIntegration(helloLambdaNodeJS);
+        const helloLambdaNodeJSResource = this.lambdaApi.root.addResource('hello-lambda');
+        helloLambdaNodeJSResource.addMethod('GET',helloLambdaNodeJSIntegration);
 
     }
 }
